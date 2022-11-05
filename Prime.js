d262b14dd84995e514dfcf52cf2c18c41e54e664
Adarshmihir6307 <adarshmihir83@gmail.com>
2022-11-05 17:20:16 +0530

    code fixed for Prime and Plindrome

--- Prime.js
+++ Prime.js
@@ -1,7 +1,31 @@
 function checkPrime(num){
-for(i=0;i<=num;i++){
-if(num%i){
-return false
-}
+
+let count_factors=0
+ 
+for(i=1;i<=num;i++){
+
+if(num%i==0){
+   
+count_factors++
+
+  }
+ 
+if(count_factors==2){
+    
 return true
+  
+   }
+    
+ return false
+  
+}
+   let ans=checkPrime(13);
+
+if(ans==true){
+    
+    console.log("Prime Number")
+   
+}else{
+  console.log("Not Prime Number")
+
 }

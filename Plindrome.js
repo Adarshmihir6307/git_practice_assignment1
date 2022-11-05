2bd89596892f4e693f34f5244ccba78982b78523
Adarshmihir6307 <adarshmihir83@gmail.com>
2022-11-05 17:27:05 +0530

    code fixed for Plindrome

--- Plindrome.js
+++ Plindrome.js
@@ -1,7 +1,17 @@
-function Plindrome(str){
+function checkPlindrome(str){
 let rev=""
  for(i=str.length-1;i>=0;i--){
      rev+=str[i]
  }
-return true
+    if(rev==str){
+      return true
+   }
+     return false
 }
+   let ans=checkPalindrome("naman")
+  if(ans==true){
+   console.log("Palindrome")  
+}else{
+
+   console.log("Not Palindrome")
+ }
